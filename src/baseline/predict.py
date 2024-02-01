016e756abcb24ed9f9071731034cf450fd6a7a9d
Valentin Belyaev <chuvalik.work@gmail.com>
2024-02-01 14:09:49 +0300

    Add scaling functions in utils.py

--- src/baseline/predict.py
+++ src/baseline/predict.py
@@ -1,9 +1,10 @@
-from ultralytics import YOLO
-import cv2
+import math
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+import cv2
+import cvzone
+from ultralytics import YOLO
+
+cap = cv2.VideoCapture("../../data/test-video.mp4")
 
 model = YOLO("../Yolo-Weights/yolov8n.pt")
 
@@ -13,8 +14,17 @@ while True:
 
     for r in results:
         for box in r.boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 255), 3)
+            x1, y1, x2, y2 = (int(c) for c in box.xyxy[0])
+            w, h = x2 - x1, y2 - y1
+
+            conf = math.ceil((box.conf[0] * 100)) / 100
+
+            cls = int(box.cls[0])
+            if cls == 0:  # Person class
+                cvzone.cornerRect(img, (x1, y1, w, h), l=9)
+
+                cx, cy = x1 + w // 2, y1 + h // 2
+                cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
---
+++ src/utils/utils.py
@@ -0,0 +1,60 @@
+import json
+from typing import Optional
+
+
+def scale_cross_lines(file_name, video_resolution) -> (list[int], list[int]):
+    """
+    Scale enter and exit cross-lines.
+    :param file_name: Json file with detections and configurations settings.
+    :param video_resolution:
+    :return:
+        List of scaled coordinates for enter cross-line,
+        List of scaled coordinates for exit cross-line,
+    """
+
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    cross_lines = _find_cross_lines_data(data)
+    # Height & width scaling.
+    width, height = video_resolution
+    cross_line_width, cross_line_height = cross_lines['box']
+
+    scaled_width = width / cross_line_width
+    scaled_height = height / cross_line_height
+
+    # Scale exit & enter cross-lines.
+    ext_line = [
+        int(x * scaled_width) if i % 2 == 0
+        else int(x * scaled_height)
+        for i, x in enumerate(cross_lines['ext_line'])
+    ]
+
+    int_line = [
+        int(x * scaled_width) if i % 2 == 0
+        else int(x * scaled_height)
+        for i, x in enumerate(cross_lines['int_line'])
+    ]
+
+    return int_line, ext_line
+
+
+def _find_cross_lines_data(data: dict) -> Optional[dict]:
+    """
+    Recursively iterate over dict and find cross-lines data.
+    :return: Dictionary with information about cross-lines.
+    """
+    if not isinstance(data, dict):
+        return None
+
+    cross_lines = None
+    for key, value in data.items():
+        if key == 'cross_lines':
+            return data[key][0]  # Cross-lines in the original json file represented as a single-element list.
+
+        if isinstance(value, dict):
+            result = _find_cross_lines_data(data[key])
+            if result is not None:
+                cross_lines = result
+
+    return cross_lines
