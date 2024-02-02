5e7ac0d71ca9e174212f970f0a3ad7f90012d910
Valentin Belyaev <chuvalik.work@gmail.com>
2024-02-02 08:57:57 +0300

    Add comments to yolo.py

--- src/predict/yolo.py
+++ src/predict/yolo.py
@@ -8,26 +8,28 @@ from ultralytics import YOLO
 from src.utils import scale_cross_lines
 from src.utils import sort
 
-# create rectangle for enter / exit
-# mask try (if time)
-
-
 cap = cv2.VideoCapture("../../data/test-video.mp4")
 cap.set(cv2.CAP_PROP_FPS, 5)
 
 model = YOLO("../Yolo-Weights/yolov8n.pt")
 
+# Video width & height
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 enter_line, exit_line = scale_cross_lines("../../data/detections.json", width, height)
 
+# Track enter & exit
 entered = {}
 exited = {}
 
 enter_count = 0
 exit_count = 0
 
+# Hardcoded value to determine entry and exit boundaries.
+cross_line_range = 5
+
+# Tracking algorithm using SORT
 tracker = sort.Sort(max_age=3, min_hits=3, iou_threshold=0.3)
 
 while True:
@@ -49,7 +51,6 @@ while True:
 
             cls = int(box.cls[0])
             if cls == 0:  # Person class
-
                 curr_detections = np.array([x1, y1, x2, y2, conf])
                 detections = np.vstack((detections, curr_detections))
 
@@ -61,10 +62,11 @@ while True:
         cvzone.cornerRect(img, (x1, y1, w, h), l=9, colorR=(255, 0, 0))
         cvzone.putTextRect(img, f"{idx}", (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
 
-        # Find a rectangle center.
+        # Tracking points (Left bottom corner of a person box)
         cx, cy = x1, y2
         cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
+        # Check if a person crossed enter & exit lines.
         if enter_line[0] < cx < enter_line[2] and enter_line[1] - 5 < cy < enter_line[3] + 5:
             entered[idx] = entered.get(idx, 0) + 1
             if idx in exited:
@@ -77,6 +79,7 @@ while True:
                 exit_count += 1
                 entered.pop(idx)
 
+    # Show current counting in a video.
     cvzone.putTextRect(img, f"Enter: {enter_count}", (10, 60), 1, 2)
     cvzone.putTextRect(img, f"Exit: {exit_count}", (10, 100), 1, 2)
 
